91973820dc98ba4f6e820889be392774792b6f3d
dengjiao <dengjiao1121@163.com>
2017-09-11 20:04:13 +0800

    首页引入各个模块，样式集成

--- src/APP.vue
+++ src/APP.vue
@@ -1,18 +1,84 @@
 <template>
     <div id="app">
 
-        <Vheader></Vheader>
+        <header></header>
 
-        <Vcontent> </Vcontent>
+        <content></content>
 
-        <AsideMenu></AsideMenu>
+        <asideMenu></asideMenu>
 
         <transition name="show">
             <Login v-if="isShowLogin"></Login>
         </transition>
 
-        <stransition name="show">
+        <transition name="show">
             <Info v-if="isShowInfo"></Info>
-        </stransition>
+        </transition>
+
+        <transition name="show">
+            <Msg v-if="isShowMsg"></Msg>
+        </transition>
+
+        <transition name="show">
+            <newArticle v-if="isShowNewArticle"></newArticle>
+        </transition>
+
+        <transition name="show">
+            <about v-if="isShowAbout"></about>
+        </transition>
     </div>
 </template>
+
+<script>
+    import header from './components/header/header.vue'
+    import content from './components/content/content.vue'
+    import AsideMenu from './components/asideMenu/asideMenu.vue'
+    import login from './components/login/login.vue'
+    import info from './components/info/info.vue'
+    import message from './components/message/message.vue'
+    import newArticle from './components/newArticle/newArticle.vue'
+    import about from './components/about/about.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            header,
+            content,
+            asideMenu,
+            login,
+            info,
+            message,
+            newArticle,
+            about
+        },
+        computed: {
+           isShowLogin(){
+               return this.$store.state.isShowLogin;
+           },
+            isShowInfo(){
+                return this.$store.state.isShowInfo;
+            },
+            isShowMsg(){
+                return this.$store.state.isShowMsg;
+            },
+            isShowNewArticle(){
+                return this.$store.state.isShowNewArticle;
+            },
+            isShowAbout(){
+                return this.$store.state.isShowAbout;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import './style/common.scss';
+
+    #app {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
